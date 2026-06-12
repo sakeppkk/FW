fc37315f638ca1dacdacb04bde418377fdccb5b9
sakeppkk <sake.ppkk.intage@gmail.com>
2026-06-12 12:24:42 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
-﻿const CACHE_NAME='fw-pwa-v2';
-const ASSETS=['./','./index.html','./viewer.html','./idcard.html','./manifest.json','./icon.svg'];
+﻿const CACHE_NAME='fw-pwa-v1';
+const ASSETS=['./','./index.html','./viewer.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('./index.html'))))});
+
 
