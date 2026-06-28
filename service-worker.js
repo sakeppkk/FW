d67ae2d5cba98f4ee0c9ee43faca5230ad1f6f56
sakeppkk <sake.ppkk.intage@gmail.com>
2026-06-28 08:43:56 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME='fw-pwa-v1';
+const CACHE_NAME='fw-pwa-v2';
 const ASSETS=['./','./index.html','./viewer.html','./ssc-camera.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
